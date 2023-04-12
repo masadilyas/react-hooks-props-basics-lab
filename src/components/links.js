d6504db5e3c57e937dfc75ece391a25890912a0d
Massad Ilyas <+>
2023-04-11 20:20:16 -0400

    completed lab

---
+++ src/components/links.js
@@ -0,0 +1,14 @@
+function links({github, linkedin}) {
+    return (
+
+     <div>
+    <h3>links</h3>
+    <a href={github }> {github}</a>
+    <a href={linkedin }> {linkedin}</a>
+    </div>
+
+    )
+
+}
+
+export default links 
--- src/data/user.js
+++ src/data/user.js
@@ -1,7 +1,7 @@
 const user = {
   name: "Liza",
   city: "New York",
-  bio: "I made this!",
+  bio: null,
   color: "firebrick",
   links: {
     github: "https://github.com/liza",
